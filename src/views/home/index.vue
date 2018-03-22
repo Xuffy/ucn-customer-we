f68bf42d700b1c8fe77d279c83055a421218a530
LAPTOP-HV2VGRGV\admin <luotao_t@yeah.net>
2018-03-22 11:04:52 +0800

    下拉组件更新

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <dropDown :list="data" />
     <!--{{$tc('page.workbench')}}-->
     <v-complex-table></v-complex-table>
     <v-simple-table></v-simple-table>
@@ -7,17 +8,58 @@
 </template>
 
 <script>
-  import {VSimpleTable, VComplexTable} from '@/components/index';
+  import { VSimpleTable, VComplexTable, dropDown } from '@/components/index';
 
   export default {
     name: 'home',
     components: {
       VSimpleTable,
-      VComplexTable
+      VComplexTable,
+      dropDown
     },
     data() {
       return {
-        url: ''
+        url: '',
+        data:[
+          {
+              title: 'parent 1',
+              expand: true,
+              selected: true,
+              children: [
+                  {
+                      title: 'parent 1-1',
+                      expand: true,
+                      children: [
+                          {
+                              title: 'leaf 1-1-1',
+                              isActive:true,
+                              disabled: false
+                          },
+                          {
+                              title: 'leaf 1-1-2',
+                              isActive:true,
+                              disabled: false
+                          }
+                      ]
+                  },
+                  {
+                      title: 'parent 1-2',
+                      expand: true,
+                      children: [
+                          {
+                              title: 'leaf 1-2-1',
+                              isActive:true,
+                              checked: false
+                          },
+                          {
+                              isActive:true,
+                              title: 'leaf 1-2-2'
+                          }
+                      ]
+                  }
+              ]
+          }
+      ]
       }
     },
     mounted() {
@@ -26,6 +68,6 @@
   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 </style>
