042dce94d2389909e62ce70229ee53d9ade0713c
raochaoyang <raozhaoyang@ucnholding.com>
2018-03-26 18:57:19 +0800

    提交order

---
+++ src/views/order/responsibility.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="responsibility">
+    order page
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'responsibility',
+        data() {
+            return {}
+        },
+        created() {},
+        methods: {}
+    }
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
