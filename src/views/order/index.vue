5c8b2ec8d3e2be05b7575ea252cac1a31378a676
raochaoyang <raozhaoyang@ucnholding.com>
2018-03-26 18:37:57 +0800

    对supplier与order的部分页面提交 ，  routerjs部分配置

--- src/views/order/index.vue
+++ src/views/order/index.vue
@@ -1,22 +1,22 @@
 <template>
-  <div>
+  <div class="orderOverview">
     order page
   </div>
 </template>
 
 <script>
-
-  export default {
-    name: 'order',
-    data() {
-      return {}
-    },
-    created() {
-    },
-    methods: {
+    export default {
+        name: 'order',
+        data() {
+            return {}
+        },
+        created() {},
+        methods: {}
     }
-  }
+
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+
 </style>
