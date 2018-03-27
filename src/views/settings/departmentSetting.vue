b314b6d5970540a55d8e2df5c0edca53b5456d95
liushuxiang <276900785@qq.com>
2018-03-27 16:07:30 +0800

    基本完成warehouse模块

---
+++ src/views/settings/departmentSetting.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "department-setting",
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
