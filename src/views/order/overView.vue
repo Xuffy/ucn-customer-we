b09695a1d80bb57e305aa261be35b6eca049a492
raochaoyang <raozhaoyang@ucnholding.com>
2018-03-27 18:45:13 +0800

    order supplier 大部分界面

---
+++ src/views/order/overView.vue
@@ -0,0 +1,157 @@
+<template>
+    <div class="orderOverview">
+        <h3 class="hd">Order Overview</h3>
+        <div class="status">
+            <div class="btn-wrap">
+                <span>Status:</span>
+                <button>TBC by Customer</button>
+                <button>TBC by Supplier</button>
+                <button>Process</button>
+                <button>Finished</button>
+                <button>Canceled</button>
+            </div>
+            <div class="select-wrap">
+                <div class="select">
+                    <el-select v-model="value" placeholder="select" @change="selectChange">
+                        <el-option
+                        v-for="item in options"
+                        :key="item.id"
+                        :label="item.label"
+                        :value="item.id" />
+                    </el-select>
+                </div>
+                <el-input v-model="keyWord" clearable prefix-icon="el-icon-search" placeholder="search" style="width:150px;"></el-input>
+            </div>
+        </div>
+        <div class="fn">
+            <div class="btn-wrap">
+                <el-button type="primary">Download Selected</el-button>
+                <el-button type="primary">Accept</el-button>
+                <el-button>Copy</el-button>
+                <el-button>Cancel</el-button>
+                <el-button>Delete</el-button>
+            </div>
+            <div class="viewBy">
+                <span>View by:</span>
+                <button>Inquiry</button>
+                <button class="active">SKU</button>
+                <div class="set">
+                    <i class="el-icon-setting"></i>
+                </div>
+            </div>
+        </div>
+        <!--form-->
+    </div>
+</template>
+<script>
+    /**
+     * @param selectChange 下拉框 值发生变更触发
+     * @param keyWord search框 值
+     * @param options 下拉框 原始数据 
+     * @param value 下拉框 选中值
+    */
+    export default {
+        name:'',
+        data() {
+            return {
+                value:'',
+                keyWord:'',
+                options: [{
+                    id: '1',
+                    label: 'Order No'
+                }, {
+                    id: '2',
+                    label: 'Sku Code'
+                },]
+            }
+        },
+        methods: {
+            selectChange(val) {
+                console.log(val)
+            }
+        }
+    }
+</script>
+<style lang="less" scoped>
+    .orderOverview {
+        .hd {
+            padding-left:15px;
+            height: 50px;
+            line-height:50px;
+            color:#666;
+            border-bottom:1px solid #ccc;
+        }
+        .status {
+            display:flex;
+            height: 60px;
+            align-items: center;
+            justify-content:space-between;
+            padding:0 15px;
+            box-sizing: border-box;
+            .btn-wrap {
+                display:flex;
+                align-items: center;
+                span {
+                    font-size:14px;
+                }
+                button {
+                    padding:2px 5px;
+                    cursor: pointer;
+                    border:1px solid #108ee9;
+                    background-color:#fff;
+                    margin-left:10px;
+                    border-radius: 5px;
+                    transition: all .5s ease;
+                    &:hover, &.active {
+                        background-color:#108ee9;
+                        color:#fff;
+                    }
+                }
+            }
+            .select-wrap {
+                display:flex;
+                align-items:center;
+                .select {
+                    width: 110px;
+                    margin-right:5px;
+                    input {}
+                }
+            }
+        }
+        .fn {
+            display:flex;
+            justify-content:space-between;
+            padding:10px 15px;
+            box-sizing: border-box;
+            .viewBy {
+                display:flex;
+                align-items: center;
+                span {
+                    font-size:14px;
+                    color:#666;
+                }
+                button {
+                    cursor: pointer;
+                    border:1px solid #108ee9;
+                    background-color:#fff;
+                    margin-left:10px;
+                    border-radius: 5px;
+                    transition: all .5s ease;
+                    padding:2px 5px;
+                    &:hover, &.active {
+                        background-color:#108ee9;
+                        color:#fff;
+                    }
+                }
+                .set {
+                    cursor: pointer;
+                    padding-left:18px;
+                    color:#999;
+                    i {
+                        font-size:25px;
+                    }
+                }
+            }
+        }
+    }
+</style>
---
+++ src/views/supplier/compare/compare.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="compare-overview">
+        <h3 class="hd">Compare</h3>
+        <div class="fn">
+            <div class="box-l">
+                <el-button type="primary">Create Order</el-button>
+                 <el-button type="primary">Accept</el-button>
+                <el-checkbox-group v-model="checkList">
+                    <el-checkbox label="Hide the Same"></el-checkbox>
+                    <el-checkbox label="Highlight the Different"></el-checkbox>
+                </el-checkbox-group>
+            </div>
+            <div>
+                <span>Compare by：</span>
+                <el-button type="primary">Inquiry</el-button>
+                <el-button type="primary">SKU</el-button>
+            </div>
+            <div class="filedSelect">
+                <span>Set Filed</span>
+                <el-select v-model="Filed" placeholder="Please select">
+                    <el-option
+                    v-for="item in options"
+                    :key="item.id"
+                    :label="item.label"
+                    :value="item.id" />
+                </el-select>
+            </div>
+            <el-button type="text">Compare History</el-button>
+        </div>
+        <!--from-->
+    </div>
+</template>
+<script>
+    export default {
+        name:'compareOverview',
+        data() {
+            return {
+                checkList: [],
+                options:[{
+                    label:'wwww.baidu.com',
+                    id:'1'
+                }],
+                Filed:''
+            }
+        }
+    }
+</script>
+<style lang="less" scoped>
+    .compare-overview {
+        .hd {
+            height:50px;
+            line-height:50px;
+            color:#666;
+            padding:0 20px;
+        }
+        .fn {
+            display:flex;
+            flex-wrap:wrap;
+            align-items: center;
+            justify-content:space-between;
+            padding:0 20px;
+            .box-l {
+                display:flex;
+                align-items: center;
+                margin:10px 0;
+                button {
+                    margin-right:10px;
+                }
+                .el-checkbox__label {
+                    font-size:12px;
+                }
+                .el-checkbox+.el-checkbox {
+                    margin-left:15px;
+                }
+            }
+            span {
+                font-size:12px;
+                color:#999;
+            }
+            button {
+                padding:5px 5px !important;
+            }
+            .filedSelect {
+                margin-left:10px;
+            }
+        }
+    }
+</style>
