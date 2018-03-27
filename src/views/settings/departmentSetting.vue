11c5e5b210d8ea50e20bb900d5a5c831f1792593
liushuxiang <276900785@qq.com>
2018-03-27 19:08:12 +0800

    添加jsx环境支持

--- src/views/settings/departmentSetting.vue
+++ src/views/settings/departmentSetting.vue
@@ -9,17 +9,29 @@
                     <div class="card">
                         <div class="title">Department</div>
                         <div class="handler">
-                            <el-button size="small" type="primary">Edit</el-button>
-                            <el-input
-                                    size="small"
-                                    class="speInput"
-                                    placeholder="请输入"
-                                    v-model="value1">
-                                <i slot="suffix" class="el-input__icon el-icon-search"></i>
-                            </el-input>
+                            <el-row>
+                                <el-col :span="8">
+                                    <el-button size="small" type="primary">Edit</el-button>
+                                </el-col>
+                                <el-col :span="16">
+                                    <el-input
+                                            size="small"
+                                            placeholder="请输入"
+                                            v-model="value1">
+                                        <i slot="suffix" class="el-input__icon el-icon-search"></i>
+                                    </el-input>
+                                </el-col>
+                            </el-row>
                         </div>
                         <div class="list">
-
+                            <el-tree
+                                    :data="data4"
+                                    show-checkbox
+                                    node-key="id"
+                                    default-expand-all
+                                    :expand-on-click-node="false"
+                                    :render-content="renderContent">
+                            </el-tree>
                         </div>
                     </div>
                 </el-col>
@@ -37,14 +49,66 @@
 <script>
     export default {
         name: "department-setting",
-        data(){
-            return{
-                value1:'',
+        data() {
+            const data = [{
+                id: 1,
+                label: '一级 1',
+                children: [{
+                    id: 4,
+                    label: '二级 1-1',
+                    children: [{
+                        id: 9,
+                        label: '三级 1-1-1'
+                    }, {
+                        id: 10,
+                        label: '三级 1-1-2'
+                    }]
+                }]
+            }, {
+                id: 2,
+                label: '一级 2',
+                children: [{
+                    id: 5,
+                    label: '二级 2-1'
+                }, {
+                    id: 6,
+                    label: '二级 2-2'
+                }]
+            }, {
+                id: 3,
+                label: '一级 3',
+                children: [{
+                    id: 7,
+                    label: '二级 3-1'
+                }, {
+                    id: 8,
+                    label: '二级 3-2'
+                }]
+            }];
+            return {
+                data4: JSON.parse(JSON.stringify(data)),
+
+                value1: '',
+
+                defaultProps: {
+                    children: 'children',
+                    label: 'id'
+                },
 
             }
-        },
-        methods:{
 
+        },
+        methods: {
+            renderContent(h, { node, data, store }) {
+                return (
+                    <span class="custom-tree-node">
+                    <span>{node.label}</span>
+                <span>
+                <el-button size="mini" type="text" on-click={ () => this.append(data) }>Append</el-button>
+                <el-button size="mini" type="text" on-click={ () => this.remove(node, data) }>Delete</el-button>
+                </span>
+                </span>);
+            }
         }
     }
 </script>
