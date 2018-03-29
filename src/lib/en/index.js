4b1f0cf5224121c385cfbbe344b71e7dab94820b
LAPTOP-HV2VGRGV\admin <luotao_t@yeah.net>
2018-03-29 09:43:04 +0800

    bug 修复

--- src/lib/en/index.js
+++ src/lib/en/index.js
@@ -1,8 +1,10 @@
 import workbench from './workbench/index.js'
 import supplier from './supplier/index.js'
 import order from './order/index.js'
+import negotiation from './negotiation';
 export default {
   workbench,
   supplier,
     order,
+    negotiation
 }
