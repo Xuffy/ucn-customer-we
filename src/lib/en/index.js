d3045739b0cbae34ff3cd4120f5dd2a61695ced7
raochaoyang <raozhaoyang@ucnholding.com>
2018-03-29 09:37:56 +0800

    国际化indexjs文件

--- src/lib/en/index.js
+++ src/lib/en/index.js
@@ -1,8 +1,13 @@
 import workbench from './workbench/index.js'
+import product from './product/index.js'
+import NEGOTIATION from './negotiation';
 import supplier from './supplier/index.js'
 import order from './order/index.js'
 export default {
   workbench,
-  supplier,
+  product,
+  NEGOTIATION,
     order,
+    supplier
 }
+
