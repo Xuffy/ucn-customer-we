18471acd6106813d06c57a1b8e34d145abdbaacb
LAPTOP-HV2VGRGV\admin <luotao_t@yeah.net>
2018-04-09 17:24:50 +0800

    修改

--- src/service/config.js
+++ src/service/config.js
@@ -2,7 +2,7 @@ export default {
   // 环境配置
   ENV: {
     local: {
-      BASE_CS: 'http://192.168.51.33:8080/web/v1/',
+      BASE_CS: 'http://192.168.51.33:8080',
       BASE_API: '',
       MOCK: 'https://www.easy-mock.com/mock/5abe0744a2146a37688ec3c1/api',
     },
