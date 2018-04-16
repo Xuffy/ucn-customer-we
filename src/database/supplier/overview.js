792e1a29fa120d2af76971d83f01e7bd1c3220a8
raochaoyang <raozhaoyang@ucnholding.com>
2018-04-16 21:01:00 +0800

    work

--- src/database/supplier/overview.js
+++ src/database/supplier/overview.js
@@ -1,1 +1,52 @@
-export default {}
+export default {
+    //overview
+    supplierName: {
+        key: 'supplierName',
+        type: 'String',
+        showType: 'text',
+        isbasic: true,
+        rule: []
+    },
+    businessScope: {
+        key: 'businessScope',
+        type: 'String',
+        showType: 'select',
+        isbasic: true,
+        rule: []
+    },
+    categoryId: {
+        key: 'categoryId',
+        type: 'String',
+        showType: 'dropdown',
+        isbasic: true,
+        rule: []
+    },
+    supplierType: {
+        key: 'supplierType',
+        type: 'String',
+        showType: 'select',
+        isbasic: false,
+        rule: []
+    },
+    SKUnameEN: {
+        key: 'SKUnameEN',
+        type: 'String',
+        showType: 'text',
+        isbasic: false,
+        rule: []
+    },
+    SKUcode: {
+        key: 'SKUcode',
+        type: 'String',
+        showType: 'text',
+        isbasic: false,
+        rule: []
+    },
+    description: {
+        key: 'description',
+        type: 'String',
+        showType: 'text',
+        isbasic: false,
+        rule: []
+    }
+}
