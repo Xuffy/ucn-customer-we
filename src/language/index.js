8c64fc19d739bd0274accc1ab3aa7d82bbf64342
raochaoyang <raozhaoyang@ucnholding.com>
2018-04-16 15:30:43 +0800

    冲突

--- src/language/index.js
+++ src/language/index.js
@@ -17,15 +17,16 @@ export default _.extend(
   { baseText: require(`./${lang}/baseText`) },
   { basePlaceholder: require(`./${lang}/basePlaceholder`) },
   { inquiry: require(`./${lang}/inquiry/index`) },
-<<<<<<< HEAD
+ require(`./${lang}/inquiry/viewByInqury.pending`),
+  require(`./${lang}/inquiry/viewBySKU.pending`),
+  require(`./${lang}/inquiry/detail/basicInfo.pending`),
+  require(`/${lang}/inquiry/create/basicInfo.pending`),
+
 
   { supplier: require(`./${lang}/supplier/overview`) } ,
    { order: require(`./${lang}/order/overview`) },
     
-=======
-  require(`./${lang}/inquiry/viewByInqury.pending`),
-  require(`./${lang}/inquiry/viewBySKU.pending`),
-  require(`./${lang}/inquiry/detail/basicInfo.pending`),
-  require(`/${lang}/inquiry/create/basicInfo.pending`)
->>>>>>> 81a409ffbb3217602702336c74e31b98bba41fa6
+
+ 
+
 )
