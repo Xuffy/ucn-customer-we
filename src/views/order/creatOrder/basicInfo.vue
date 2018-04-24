81a07c168e089a22abea70cf32bacb8380e3b076
raochaoyang <raozhaoyang@ucnholding.com>
2018-04-24 20:18:19 +0800

    work

--- src/views/order/creatOrder/basicInfo.vue
+++ src/views/order/creatOrder/basicInfo.vue
@@ -3,206 +3,71 @@
              <div class="basicinfo_title">basicInfo</div>
              <div class="basicinfo_form">
                   <el-form ref='ruleform' :model="formItem" label-width="230px" :rules="rules" >
-                    <el-row :gutter="10">                    
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8"                                                 
+                    <el-row>
+                     <el-col :xs="24" :sm="12" :md="12" :lg="8" 
+                           v-for="(item, index) in $db.order.basicInfo"
+                            :key="index"                         
                            >
+                            <el-form-item 
+                                  v-if='item.type=="text"'
+                                 :label="item.label" 
+                                 :prop="item.key">
+                                <el-input                       
+                                v-model='formItem[item.key]'                                   :disabled=item.ismodify||disabled||item.isDefaultEdit  placeholder=""></el-input>
+                            </el-form-item>  
                              <el-form-item 
-                                    :label="$i.orderNo" 
-                                     prop="orderNo">
-                                    <el-input                       
-                                    v-model="formItem.orderNo"       
-                                    :disabled=true  placeholder=""></el-input>
-                                </el-form-item>                                                        
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item  :label="$i.orderDate" prop="orderDate">
-                              <div style='display:flex;max-width:200px;'>
-                                       <el-date-picker
-                                              v-model="formItem.orderDate"
-                                              type="date"
-                                              :disabled=disabled||podisabled
-                                              placeholder=""
-                                              >
-                                        </el-date-picker>
-                                    </div>
-                                </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                                <el-form-item :label="$i.customerOrderNo" prop="customerOrderNo">
-                                    <el-input v-model="formItem.customerOrderNo" :disabled='disabled' placeholder=""></el-input>
-                                </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                                <el-form-item class="form-list" :label="$i.customerName" prop="customerName">
-                                    <el-input v-model="formItem.customerName" :disabled='disabled' placeholder=""></el-input>                   
-                                </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.customerNo" prop="customerNo">
-                                 <el-input v-model="formItem.customerNo" :disabled='disabled' placeholder="">
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>                 
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.supplierOrderNo" prop="supplierOrderNo">
-                                 <el-input v-model="formItem.supplierOrderNo" :disabled='disabled' placeholder="select">
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.supplierName" prop="supplierName">
-                                 <el-input v-model="formItem.supplierName"  :disabled='disabled||podisabled'  placeholder="">
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>              
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.supplierNo" prop="supplierNo">
-                                 <el-input v-model="formItem.supplierNo" :disabled=true placeholder="">
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.QuotationNo" prop="quotationNo">
-                                 <el-input v-model="formItem.quotationNo" :disabled=true placeholder="">
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.orderStatus" prop="status">
-                                 <el-select v-model="formItem.status" :disabled='disabled' placeholder="select">
-<!--
-                                    <el-option
-                                      v-for="item in orderStatus"
-                                      :key="item.value"
-                                      :label="item.label"
-                                      :value="item.value">
-                                    </el-option>
--->
-                                  </el-select>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.deliveryDate" prop="deliveryDt">
-                             <div style='display:flex;max-width:200px;'>
-                            <el-date-picker
-                                      v-model="formItem.deliveryDt"
-                                      type="date"
-                                      :disabled='disabled'
-                                      placeholder="选择日期">
-                                    </el-date-picker>
-                                    </div>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.incoterm" prop="incoterm">
-                                 <el-select v-model="formItem.incoterm" :disabled='disabled' placeholder="select">
-<!--
-                                    <el-option
-                                      v-for="item in priceTerms"
-                                      :key="item.value"
-                                      :label="item.label"
-                                      :value="item.value">
-                                    </el-option>
--->
-                                  </el-select>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.incortermAea" prop="incortermAea">
-                                 <el-input v-model="formItem.incortermAea" :disabled='disabled' placeholder="select">                               
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.payment" prop="payment">
-                                 <el-input v-model="formItem.payment" :disabled='disabled' placeholder="select">
-                                    
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.lcNo" prop="lcNo">
-                                 <el-input v-model="formItem.lcNo" :disabled='disabled' placeholder="select">
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.paymentDays" prop="paymentDays">
-                                 <el-input v-model="formItem.paymentDays" :disabled='disabled' placeholder="select">
-                                   
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.paymentStatus" prop="paymentStatus">
-                                 <el-input v-model="formItem.paymentStatus" :disabled=true placeholder="">
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.departureCountry" prop="departureCountry">
-                                 <el-input v-model="formItem.departureCountry" :disabled='disabled' placeholder="Please enter">
-                                   
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.departurePort" prop="departurePort">
-                                 <el-input v-model="formItem.departurePort" :disabled='disabled' placeholder="select">
-                                   
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.destinationCountry" prop="destinationCountry">
-                                 <el-input v-model="formItem.destinationCountry" :disabled='disabled' placeholder="select">
-                                   
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.destinationPort" prop="destinationPort">
-                                 <el-input v-model="formItem.destinationPort" :disabled='disabled' placeholder="select">
-                                   
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>                        
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.transportationWay" prop="transport">
-                                 <el-select v-model="formItem.transport" :disabled=true placeholder="select">
-                                    
-                                  </el-select>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.customerAgreementNumber" prop="customerAgreementNo">
-                                 <el-input v-model="formItem.customerAgreementNo" :disabled='disabled' placeholder="select">
-                                   
-                                  </el-input>
-                            </el-form-item>
-                         </el-col>
-                          <el-col :xs="24" :sm="12" :md="12" :lg="8">
-                            <el-form-item class="form-list" :label=" $i.customerAgreementDate" prop="customerAgreementDt">
+                                  v-if='item.type=="textdialog"'
+                                 :label="item.label" 
+                                 :prop="item.key">
+                                <el-input                       
+                                v-model='formItem[item.key]'                                   :disabled=item.ismodify||disabled||item.isDefaultEdit  placeholder=""></el-input>
+                                <i class="el-icon-more-outline" style='fontSize:20px' @click='dialogEdit'></i>
+                            </el-form-item>                         
+                             <el-form-item class="form-list"
+                                   v-if='item.type=="datepicker"'
+                                    :label="item.label" 
+                                   :prop="item.key">
                                    <div style='display:flex;max-width:200px;'>
                                    <el-date-picker
-                                      v-model="formItem.customerAgreementDt"
+                                      v-model='formItem[item.key]' 
                                       type="date"
-                                      :disabled='disabled'
-                                      placeholder="选择日期">
+                                     :disabled=item.ismodify||disabled||item.isDefaultEdit
+                                    >
                                     </el-date-picker>
-                                        </div>
+                                      </div>
                             </el-form-item>
-                         </el-col>  
-                          <el-col :xs="24" :sm="24" :md="24" :lg="24">
-                                <el-form-item class="form-list" :label=" $i.remark" prop="remark">
-                                    <el-input type="textarea" v-model="formItem.remark" :disabled='disabled' placeholder="Enter something..."></el-input>
+                            
+                             <el-form-item 
+                                  v-if='item.type=="select"'
+                                 :label="item.label"
+                                 :prop="item.key">
+                                 <el-select
+                                          v-model='formItem[item.key]'                 
+                                        :disabled=item.ismodify||disabled||item.isDefaultEdit >
+                                         <el-option>
+                                        </el-option>
+                                  </el-select>
+                            </el-form-item>                  
+                         </el-col>
+
+                         <el-col :xs="24" :sm="24" :md="24" :lg="24">
+                                <el-form-item class="form-list" 
+                                   v-for="(item, index) in $db.order.basicInfo"
+                                   :key="index"
+                                   v-if='item.type==="textarea"'
+                                   :label="item.label" 
+                                   :prop="item.key">
+                                    <el-input type="textarea"
+                                      v-model='formItem[item.key]'                  :disabled=item.ismodify||disabled||item.isDefaultEdit                   
+                                     ></el-input>
                                 </el-form-item>
-                            </el-col>
-
-                    </el-row>
-                </el-form>
+                          </el-col>
+                     </el-row>
+                 </el-form>
              </div>
+               <el-dialog :visible.sync="dialogEditDiv" width='70%'>
 
+                </el-dialog>
          </div>  
 </template>
 <script>
@@ -230,15 +95,17 @@
         },
         data() {
             return {
+                dialogEditDiv: false,
                 formItem: {
-                    orderNo: '555555', //必填   系统生成 不可编辑
+                    timeZone: 0, //.........时区
+                    orderNo: '8222888888', //必填   系统生成 不可编辑
                     orderDate: '', //必填    系统生成   可编辑    ??????
                     customerOrderNo: '',
-                    customerName: '', //必填 系统生成 弹出框 
-                    customerNo: '', //必填 系统生成 弹出框
+                    customerName: '', //必填 系统生成  
+                    customerNo: '', //必填 系统生成 
                     supplierOrderNo: '',
-                    supplierName: '', //必填 不可编辑 系统生成 
-                    supplierNo: '', //必填 不可编辑 系统生成 
+                    supplierName: '', //必填 不可编辑 系统生成 弹出框
+                    supplierNo: '', //必填 不可编辑 系统生成 弹出框
                     quotationNo: '', // 不可编辑
                     status: '', //必填 orderStatus下拉框值 部分可编辑.........  可手动finished
                     deliveryDt: '', //必填 
@@ -256,9 +123,6 @@
                     customerAgreementNo: '',
                     customerAgreementDt: '',
                     remark: '',
-                    
-                    
-                    
                 },
                 //......................表单正则
                 rules: {
@@ -313,30 +177,6 @@
                         trigger: 'blur'
                     }, ],
                 },
-                //                referencesInquiryID: [{
-                //                    value: 'TBCByCustomer',
-                //                    label:
-                //                }, {
-                //                    value: 'TBCBySupplier',
-                //                    label:
-                //                }],
-                //                orderStatus: [{
-                //                    value: 'Process',
-                //                    label:
-                //                }, {
-                //                    value: 'Finished',
-                //                    label:
-                //                }, {
-                //                    value: 'Cancled',
-                //                    label:
-                //                }],
-                //                priceTerms: [{
-                //                    value: 'fob',
-                //                    label:
-                //                }, {
-                //                    value: 'exw',
-                //                    label:
-                //                }],
             }
         },
         methods: {
@@ -347,6 +187,10 @@
                     }
                 });
             },
+            dialogEdit() {
+                this.dialogEditDiv = true;
+
+            }
         },
         mounted() {
 
--- src/views/order/creatOrder/caculate.vue
+++ src/views/order/creatOrder/caculate.vue
@@ -1,37 +1,37 @@
 <template>
   <div class='caculate'>
-      <div class="caculate_title">Caculate</div>
+      <div class="caculate_title">Summary</div>
       <el-form :model="caculateForm" label-width="190px" class='caculateForm'>
                 <el-row class="speZone">
                     <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
                         <el-form-item prop="email" label="Total Number of Carton">                          
-                            <el-input size="mini" v-model="caculateForm.totalNumberofCarton" :disabled='disabled'></el-input>
+                            <el-input size="mini" v-model="caculateForm.totalNumberofCarton" :disabled=true></el-input>
                         </el-form-item>                                         
                     </el-col>
                     <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
                          <el-form-item prop="email" label="Total Quantity">                          
-                            <el-input size="mini" v-model="caculateForm.totalQuantity" :disabled='disabled'></el-input>
+                            <el-input size="mini" v-model="caculateForm.totalQuantity" :disabled=true></el-input>
                         </el-form-item>
                     </el-col>
                    
                     <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
                         <el-form-item prop="email" label="Total Volume">                          
-                            <el-input size="mini" v-model="caculateForm.totalVolume" :disabled='disabled'></el-input>
+                            <el-input size="mini" v-model="caculateForm.totalVolume" :disabled=true></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
                          <el-form-item prop="email" label="Total Net Weight">                          
-                            <el-input size="mini" v-model="caculateForm.totalNetWeight" :disabled='disabled'></el-input>
+                            <el-input size="mini" v-model="caculateForm.totalNetWeight" :disabled=true></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
                         <el-form-item prop="email" label="Total Gross Weight">                          
-                            <el-input size="mini" v-model="caculateForm.totalGrossWeight" :disabled='disabled'></el-input>
+                            <el-input size="mini" v-model="caculateForm.totalGrossWeight" :disabled=true></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
                          <el-form-item prop="email" label="Quantity of SKU">                          
-                            <el-input size="mini" v-model="caculateForm.quantityofSKU" :disabled='disabled'></el-input>
+                            <el-input size="mini" v-model="caculateForm.quantityofSKU" :disabled=true></el-input>
                         </el-form-item>
                     </el-col>
                 </el-row>
@@ -40,45 +40,47 @@
 </template>
 
 <script>
-  export default {
-    name: 'caculate',
-    data() {
-      return {
-          caculateForm:{
-             totalNumberofCarton:'',
-              totalQuantity:'',
-              totalVolume:'',
-              totalNetWeight:'',
-              totalGrossWeight:'',
-              quantityofSKU:''
-          }
+    export default {
+        name: 'caculate',
+        data() {
+            return {
+                caculateForm: {
+                    totalNumberofCarton: '',
+                    totalQuantity: '',
+                    totalVolume: '',
+                    totalNetWeight: '',
+                    totalGrossWeight: '',
+                    quantityofSKU: ''
+                }
 
-      }
-    },
-    props:{
-         disabled: {
+            }
+        },
+        props: {
+            disabled: {
                 type: Boolean,
                 default: false
             },
-    },
-    created() {
-    },
-    methods: {
+        },
+        created() {},
+        methods: {
 
-    },
-  }
+        },
+    }
+
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     .caculate_title {
-       font-size: 14px;
+        font-size: 14px;
         height: 60px;
         line-height: 60px;
         border-bottom: 1px solid #ccc;
         padding: 0 15px;
     }
-    .caculateForm{
+
+    .caculateForm {
         margin-top: 20px;
         margin-bottom: 80px;
     }
+
 </style>
