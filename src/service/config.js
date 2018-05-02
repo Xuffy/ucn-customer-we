c2bae515d097d9b760a74b909d629fc857bf75a0
徐飞 <d.xuffy@qq.com>
2018-05-02 10:01:26 +0800

    修改

--- src/service/config.js
+++ src/service/config.js
@@ -42,7 +42,6 @@ export default {
       USERVICE_URL: 'http://dev.uservice.ucnholding.com:18100', // 服务商端
       UBIZLOG_URL: 'http://dev.ubizlog.ucnholding.com:18100', // 业务日志
       UWAREHOUSE_URL: 'http://dev.uwarehouse.ucnholding.com:18100',
-      LOGIN_URL: 'http://dev.auth.ucnscm.cn', // 登录授权
     },
     develop: {
       BASE_URL: 'http://dev.upurchase-facade.ucnholding.com:18100', // 采购端
@@ -57,7 +56,7 @@ export default {
       USERVICE_URL: 'http://dev.uservice.ucnholding.com:18100', // 服务商端
       UBIZLOG_URL: 'http://dev.ubizlog.ucnholding.com:18100', // 业务日志
       UWAREHOUSE_URL: 'http://dev.uwarehouse.ucnholding.com:18100',
-      LOGIN_URL: 'http://dev.auth.ucnscm.cn', // 登录授权
+      LOGIN_URL: 'http://dev.auth.ucnscm.cn/#/?type=1&redirect=', // 登录授权
     },
     test: {
       BASE_API: 'http://store.test.tailorx.cn/api'
