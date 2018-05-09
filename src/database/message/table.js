4a27557c661689c1242d010c1b4c7de98aa8b247
LAPTOP-Q5IGJGU8\admin <993826418@qq.com>
2018-05-09 16:30:06 +0800

    message 字段不齐

--- src/database/message/table.js
+++ src/database/message/table.js
@@ -1,14 +1,19 @@
 export default {
-    title: {
-      key: 'title'
-    },
-    content: {
-      key: 'content'
-    },
+  title: {
+    key: 'title'
+  },
+  content: {
+    key: 'content'
+  },
   sendTime: {
-      key: 'sendTime',
-      type:'textDate'
-    },
+    key: 'sendTime'
+  },
+  read: {
+    key: 'isRead'
+  },
+  sender: {
+    key: 'senderName'
+  },
   subscribeId: {
     key: 'subscribeId',
     _hide:true,
