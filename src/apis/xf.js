0af29e2c984ccfeede6f20708140ddb91116ca2b
徐飞 <d.xuffy@qq.com>
2018-05-23 16:45:42 +0800

    修改

--- src/apis/xf.js
+++ src/apis/xf.js
@@ -4,7 +4,8 @@ export default {
   USER_PRIVILEGE: ['/user/privilege', 'UUSER_URL'],
   UTASK_PAGELIST: ['/utask/pageList', 'UWORKBENCH_URL'],
   UTASK_TYPELIST: ['/utask/typeList', 'UWORKBENCH_URL'],
-  UDA_FINDDATAANALYSISLIST: ['/uda/findDataAnalysisList', 'BASE_JY', 'UWORKBENCH_URL'],
+  UTASK_GETBYTYPEANDMODULE: ['/utask/getByTypeAndModule', 'UWORKBENCH_URL'],
+  UDA_FINDDATAANALYSISLIST: ['/uda/findDataAnalysisList', 'UWORKBENCH_URL'],
   ITEMFAVORITE_PART: ['/itemfavorite/part', 'UMETA_URL'],
   ITEMFAVORITE_UPDATE: ['/itemfavorite/update', 'UMETA_URL'],
   CHATMESSAGE_QUERY: ['/chatmessage/query', 'UMESSAGE_URL'], // 获取留言板
