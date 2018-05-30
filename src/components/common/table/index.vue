e828b0c4b79276db070736e85c02f591209464fe
liushuxiang <276900785@qq.com>
2018-05-30 10:45:49 +0800

    xx

--- src/components/common/table/index.vue
+++ src/components/common/table/index.vue
@@ -285,6 +285,7 @@
         });
       },
       getImage(value, split = ',') {
+          console.log(value,'value')
         if (_.isEmpty(value)) return false;
 
         if (_.isString(value)) {
