47acb691b67e0c8cf50c5721d13ac0e8cfce6b51
raochaoyang <raozhaoyang@ucnholding.com>
2018-06-12 15:10:04 +0800

    no message

--- src/service/config.js
+++ src/service/config.js
@@ -3,8 +3,6 @@ export default {
   ENV: {
     local: {
       MOCK: 'https://www.easy-mock.com/mock/5abe0744a2146a37688ec3c1/api',
-
-      a_localtion:'//192.168.51.219:11001',
       LOGIN_URL: 'http://192.168.51.138:8070',
       // LOGIN_URL: '//dev.auth.ucnscm.cn',
       BASE_URL: '//192.168.51.148:11001', // 采购端
