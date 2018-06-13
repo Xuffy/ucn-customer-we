be27c2cc83e8e4192cc439ca87b7827f8089deb1
徐飞 <d.xuffy@qq.com>
2018-06-13 11:06:20 +0800

    添加表单验证

--- src/service/util.js
+++ src/service/util.js
@@ -91,6 +91,88 @@ export default {
       return (val / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
     }
 
+
+    /**
+     *
+     * @param data
+     * @param db
+     * {
+     *   type:'Number', // 数据类型：
+     *   required:true, // 必填项
+     *   length:20, // 最大长度
+     *   minLength:20, // 最小长度
+     * }
+     * @returns {boolean}
+     */
+    Vue.prototype.$validateForm = (data, db) => {
+
+      for (let k in data) {
+        let val = (data[k])
+          , item = db[k]
+          , key = item.key || true
+          , validate;
+
+        if (_.isEmpty(item) || !_.isObject(item._rules)) continue;
+
+        validate = item._rules;
+
+        if (validate.required && !val) {
+          Message.warning(`请填写 ${item.label}`);
+          return key;
+        }
+
+        switch (validate.type) {
+          case 'Number':
+            if (_.isRegExp(validate.reg) && !validate.reg.test(val)) {
+              Message.warning(`${item.label} 格式不正确`);
+              return key;
+            }
+            if (!Number(val) || !_.isNumber(Number(val))) {
+              Message.warning(`${item.label} 请填写数字`);
+              return key;
+            }
+            if(validate.max && validate.max < val){
+              Message.warning(`${item.label} 值不能大于${validate.max}`);
+              return key;
+            }
+            if(validate.min && validate.min > val){
+              Message.warning(`${item.label} 值不能小于${validate.max}`);
+              return key;
+            }
+            break;
+          case 'Email':
+            if (!/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(val)) {
+              Message.warning(`${item.label} 请填写邮箱`);
+              return key;
+            }
+            break;
+          case 'Regexp':
+            if (_.isRegExp(validate.reg) && !validate.reg.test(val)) {
+              Message.warning(item.label || `${item.label} 格式不正确`);
+              return key;
+            }
+            break;
+        }
+
+        if (val){
+          if (validate.length && validate.length < val.length) {
+            Message.warning(`${item.label} 长度不能大于${validate.length}位`);
+            return key;
+          }
+
+          if (validate.minLength && validate.minLength > val.length) {
+            Message.warning(`${item.label} 长度不能小于${validate.length}位`);
+            return key;
+          }
+        }
+
+      }
+
+      return false;
+
+    }
+
+
     /**
      * table 数据过滤
      * @type {{contrast(*=, *=): *, setHighlight(*=): *, setHideSame(*=): *}}
@@ -141,6 +223,7 @@ export default {
         });
       }
     };
+
     /**
      * 删除带有前缀下划线数据
      */
@@ -166,6 +249,7 @@ export default {
       if (_.isArray(list)) _.map(list, fieldRemark, details);
       return list;
     };
+
     /**
      * 时区转换 传入时区 如 0 8 -1
      */
