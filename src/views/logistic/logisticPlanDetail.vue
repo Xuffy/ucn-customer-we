8c5d264f02237a4de50b74e13b494ad9553e02a7
raochaoyang <raozhaoyang@ucnholding.com>
2018-06-21 09:43:39 +0800

    删除 payment 功能

--- src/views/logistic/logisticPlanDetail.vue
+++ src/views/logistic/logisticPlanDetail.vue
@@ -414,7 +414,10 @@ export default {
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        this.selectionContainer.forEach(i => this.arraySplite(this.containerInfo, i))
+        this.oldPlanObject.rmContainerDetail = this.selectionContainer.map((item)=>{
+          return item.id;
+        })
+        this.containerInfo = _.difference(this.containerInfo,this.selectionContainer);
         this.$message({
           type: 'success',
           message: '删除成功!'
