da39b0afbbabab60d3cccf5ab2f457c2530362f1
dhc <dhc@ucnholding.com>
2018-06-27 17:27:36 +0800

    询价单对比详细不同的高亮项默认选中

--- src/views/negotiation/compare.vue
+++ src/views/negotiation/compare.vue
@@ -72,7 +72,7 @@ export default {
       Filed: '',
       compareBy: 0,
       hideSame: false,
-      highLight: false,
+      highLight: true,
       checkedArg: [],
       compareType: '',
       params: {
