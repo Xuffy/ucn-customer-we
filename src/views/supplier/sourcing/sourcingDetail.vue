75e6882f2b4d707631e978476b9f34278ee7b8ad
唐文娟 <993826418@qq.com>
2018-06-28 19:57:35 +0800

    更改

--- src/views/supplier/sourcing/sourcingDetail.vue
+++ src/views/supplier/sourcing/sourcingDetail.vue
@@ -10,7 +10,7 @@
                     <el-row>
                       <el-row>
                         <el-col :span="4">
-                          <v-image :src="basicDate.logo" style="height: 273px;"/>
+                          <v-image :src="basicDate.logo" style="height: 184px;"/>
                         </el-col>
                         <el-col :span="20">
                           <el-form>
