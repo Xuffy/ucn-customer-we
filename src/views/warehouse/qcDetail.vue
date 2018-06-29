de94c76049877e2819dbef8c8a903d25bf913fe3
liushuxiang <276900785@qq.com>
2018-06-29 15:42:28 +0800

    xx

--- src/views/warehouse/qcDetail.vue
+++ src/views/warehouse/qcDetail.vue
@@ -485,6 +485,7 @@
                 lengthOption:[],
                 volumeOption:[],
                 weightOption:[],
+                pbCodeOption:[],
 
 
             }
@@ -525,10 +526,17 @@
                             e._disabled=true;
                         }
                         e.deliveryDate._value=this.$dateFormat(e.deliveryDate.value,'yyyy-mm-dd');
-                        e.skuUnitDictCode._value=_.findWhere(this.skuUnitOption,{code:e.skuUnitDictCode.value}).name;
-                        e.volumeUnitDictCode._value=_.findWhere(this.volumeOption,{code:e.volumeUnitDictCode.value}).name;
-                        e.weightUnitDictCode._value=_.findWhere(this.weightOption,{code:e.weightUnitDictCode.value}).name;
-                        e.lengthUnitDictCode._value=_.findWhere(this.lengthOption,{code:e.lengthUnitDictCode.value}).name;
+                        e.skuUnitDictCode._value=e.skuUnitDictCode.value?_.findWhere(this.skuUnitOption,{code:e.skuUnitDictCode.value}).name:'';
+                        e.volumeUnitDictCode._value=e.volumeUnitDictCode.value?_.findWhere(this.volumeOption,{code:e.volumeUnitDictCode.value}).name:'';
+                        e.weightUnitDictCode._value=e.weightUnitDictCode.value?_.findWhere(this.weightOption,{code:e.weightUnitDictCode.value}).name:'';
+                        e.lengthUnitDictCode._value=e.lengthUnitDictCode.value?_.findWhere(this.lengthOption,{code:e.lengthUnitDictCode.value}).name:'';
+
+                        e.skuBarCodeResultDictCode._value=e.skuBarCodeResultDictCode.value?_.findWhere(this.pbCodeOption,{code:e.skuBarCodeResultDictCode.value}).name:'';
+                        e.innerPackingBarCodeResultDictCode._value=e.innerPackingBarCodeResultDictCode.value?_.findWhere(this.pbCodeOption,{code:e.innerPackingBarCodeResultDictCode.value}).name:'';
+                        e.skuLabelResultDictCode._value=e.skuLabelResultDictCode.value?_.findWhere(this.pbCodeOption,{code:e.skuLabelResultDictCode.value}).name:'';
+                        e.outerCartonBarCodeResultDictCode._value=e.outerCartonBarCodeResultDictCode.value?_.findWhere(this.pbCodeOption,{code:e.outerCartonBarCodeResultDictCode.value}).name:'';
+                        e.shippingMarkResultDictCode._value=e.shippingMarkResultDictCode.value?_.findWhere(this.pbCodeOption,{code:e.shippingMarkResultDictCode.value}).name:'';
+
                         return e;
                     });
                     let diffData=[];
@@ -972,9 +980,8 @@
             this.$ajax.get(this.$apis.get_allUnit).then(res=>{
                 console.log(res)
             })
-            this.$ajax.post(this.$apis.get_partUnit,['QC_TYPE','QC_MD','SKU_UNIT','LH_UNIT','VE_UNIT','WT_UNIT'],{cache:true})
+            this.$ajax.post(this.$apis.get_partUnit,['QC_TYPE','QC_MD','SKU_UNIT','LH_UNIT','VE_UNIT','WT_UNIT','PB_CODE'],{cache:true})
                 .then(res=>{
-                    console.log(res,'res')
                     res.forEach(v=>{
                         if(v.code==='QC_TYPE'){
                             this.qcTypeOption=v.codes;
@@ -988,6 +995,9 @@
                             this.volumeOption=v.codes;
                         }else if(v.code==='WT_UNIT'){
                             this.weightOption=v.codes;
+                        }else if(v.code==='PB_CODE'){
+                            this.pbCodeOption=v.codes;
+                            console.log(this.pbCodeOption,'pbCodeOption')
                         }
                     });
                     this.getQcOrderDetail();
