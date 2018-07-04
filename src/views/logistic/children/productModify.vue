2343878a36c542f673d9e6cd506bffd1e112f5d0
dengfukang <dengfukang@ucnholding.com>
2018-07-04 18:03:01 +0800

    no message

--- src/views/logistic/children/productModify.vue
+++ src/views/logistic/children/productModify.vue
@@ -1,10 +1,15 @@
 <template>
   <el-row>
     <!--<v-table-filter :hideFilterValue="true" class="filter"/>-->
-    <el-table :data="tableData" style="width: 100%" class="table" max-height="400">
-      <!-- <el-table-column :label="$i.logistic.negotiate" width="120" align="center">
+    <el-table :data="tableData" style="width: 100%" class="table" max-height="400"> 
+      <el-table-column :label="$i.logistic.entryDt" width="150" align="center" sortable>
         <template slot-scope="scope">
-          <span>{{ scope.row.entryDt ? $dateFormat(scope.row.entryDt.value, 'yyyy-mm-dd') : null }}</span>
+           <span>{{ scope.row.entryDt ? $dateFormat(scope.row.entryDt.value, 'yyyy-mm-dd hh:mm') : null }}</span>
+        </template>
+      </el-table-column>
+      <!-- <el-table-column :label="$i.logistic.entryName" width="120" align="center" sortable>
+        <template slot-scope="scope">
+          <span>{{ scope.row.entryName.value }}</span>
         </template>
       </el-table-column> -->
       <el-table-column :label="$i.logistic.orderNo" width="100" align="center" sortable>
