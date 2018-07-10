211b60be81a15de9c9086feb058376c9ce21ae39
徐飞 <d.xuffy@qq.com>
2018-07-10 17:18:15 +0800

    修改

--- src/service/fetch.js
+++ src/service/fetch.js
@@ -7,7 +7,7 @@ const $ajax = new ajax();
 export default {
 
   export_taske(templateCode, params = {}) {
-    return $ajax.post($apis.EXPORTFILE_EXPORTTASKE, {templateCode, arguments: params})
+    return $ajax.post($apis.EXPORTFILE_EXPORTTASKE, {templateCode, arguments: JSON.stringify(params)})
       .then(res => {
         Message.success('导出任务正在执行，请到任务中心查看');
       });
