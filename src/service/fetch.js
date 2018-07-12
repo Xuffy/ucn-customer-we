7dbdda6831119a8c4c72f22e30e760d970fb5bc9
徐飞 <d.xuffy@qq.com>
2018-07-12 16:25:45 +0800

    修改

--- src/service/fetch.js
+++ src/service/fetch.js
@@ -1,6 +1,9 @@
 import ajax from 'service/ajax';
 import $apis from '../apis/index'
-import {Message} from 'element-ui';
+import {MessageBox} from 'element-ui';
+import $i from '../language/index';
+import router from './router'
+import Vue from 'vue'
 
 const $ajax = new ajax();
 
@@ -8,7 +11,13 @@ export default {
   export_task(templateCode, params = {}) {
     return $ajax.post($apis.EXPORTFILE_EXPORTTASKE, {templateCode, arguments: JSON.stringify(params)})
       .then(res => {
-        Message.success('导出任务正在执行，请到任务中心查看');
+        MessageBox.confirm($i.logs.exportSuccess, $i.hintMessage.systemHints, {
+          confirmButtonText: $i.button.confirm,
+          cancelButtonText: $i.button.ccancel,
+          type: 'success'
+        }).then(() => {
+          router.push({path: '/logs/task', query: {type: 'export'}})
+        }).catch(e => e);
       });
   }
 };
