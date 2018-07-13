0801a4cb5ab2822fc71029ad21156dcb9a5763cd
徐飞 <d.xuffy@qq.com>
2018-07-13 17:35:44 +0800

    修改

--- src/components/common/historyModify/index.vue
+++ src/components/common/historyModify/index.vue
@@ -22,6 +22,7 @@
                          v-if="(!item._hide && !item._hidden) || item._title"
                          min-width="200px"
                          :prop="item.key"
+                         :cell-style="item._style || {}"
                          :label="item.label">
 
           <template slot-scope="{ row }" v-if="row[item.key] && !row[item.key]._hide">
