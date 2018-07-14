28028954915ed7618b97033a01364da763a8c888
徐飞 <d.xuffy@qq.com>
2018-07-14 14:30:30 +0800

    修改

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -24,8 +24,9 @@
       let params = Qs.stringify({
         type: config.CLIENT_TYPE,
         language: config.LANGUAGE,
-        redirect: Base64.encode(`${window.location.origin}/static/authorize/index.html`)
+        redirect: Base64.encode(`//${window.location.host}/static/authorize/index.html`)
       });
+
       let redirectUrl = `?${params}`;
       this.$message.closeAll();
       this.loginUrl = config.ENV.LOGIN_URL + redirectUrl;
