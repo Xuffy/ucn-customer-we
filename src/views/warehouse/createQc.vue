9d9cbf8f23f9370ca0d6693c277d4a4d3edc557a
liushuxiang <276900785@qq.com>
2018-08-01 11:19:22 +0800

    xx

--- src/views/warehouse/createQc.vue
+++ src/views/warehouse/createQc.vue
@@ -770,7 +770,6 @@
         mounted() {
             this.loadingData = true;
             this.columnConfig = this.$db.warehouse.createQcProductTable;
-            console.log(this.columnConfig)
         },
         watch: {
             selectProductTableData(n) {
